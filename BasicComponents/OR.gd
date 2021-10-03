364181bddf21d7b34e8b4ca3bf11d1f3844d8ee3
Joshua Patterson <jleegippies@gmail.com>
2021-10-03 13:14:10 -0500

    Added lines to AND, OR, and XOR

--- BasicComponents/OR.gd
+++ BasicComponents/OR.gd
@@ -2,6 +2,18 @@ extends Node2D
 
 func operate(var input_1, var input_2):
 	var output_1 = $NAND.operate(input_1, input_1)
+	
+	if output_1:
+		$Inter1.default_color = Color.green
+	else:
+		$Inter1.default_color = Color.red
+	
 	var output_2 = $NAND2.operate(input_2, input_2)
+	
+	if output_2:
+		$Inter2.default_color = Color.green
+	else:
+		$Inter2.default_color = Color.red
+	
 	var output = $NAND3.operate(output_1, output_2)
 	return output
--- BasicComponents/XOR.gd
+++ BasicComponents/XOR.gd
@@ -2,6 +2,18 @@ extends Node2D
 
 func operate(var input_1, var input_2):
 	var output_1 = $OR.operate(input_1, input_2)
+	
+	if output_1:
+		$Inter1.default_color = Color.green
+	else:
+		$Inter1.default_color = Color.red
+	
 	var output_2 = $NAND.operate(input_1, input_2)
+	
+	if output_2:
+		$Inter2.default_color = Color.green
+	else:
+		$Inter2.default_color = Color.red
+	
 	var output = $AND.operate(output_1, output_2)
 	return output
